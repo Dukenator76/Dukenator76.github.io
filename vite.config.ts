bf93dba43aecdb6982d152bf3198e90f3b0af506
Dukenator76 <ducatimondani@gmail.com>
2024-11-25 15:57:52 -0800

    Add MIME type configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,28 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'configure-response-headers',
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Content-Type', 'application/javascript');
+          next();
+        });
+      }
+    }
+  ],
   base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
   }
 })
