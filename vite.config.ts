012fc62ca8a19a21545abb5e08df0459cf382c85
Dukenator76 <ducatimondani@gmail.com>
2024-11-25 14:16:40 -0800

    Simplify configuration and add proper headers

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,6 @@ export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Cache-Control': 'no-store',
-      'X-Content-Type-Options': 'nosniff'
-    }
+    assetsDir: 'assets'
   }
 })
