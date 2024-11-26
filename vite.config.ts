1006ff1a96e3474893e94b256a190788cb86a2ab
Dukenator76 <ducatimondani@gmail.com>
2024-11-25 16:55:26 -0800

    chore: Simplify configuration to basics

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'configure-response-headers',
-      configureServer(server) {
-        server.middlewares.use((_req, res, next) => {
-          res.setHeader('Content-Type', 'application/javascript');
-          next();
-        });
-      }
-    }
-  ],
-  base: './',
+  plugins: [react()],
+  base: '/Website/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    outDir: 'dist'
   }
 })
