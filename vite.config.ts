24731a28ffff0f106d7ab478345dc902853ae316
Dukenator76 <ducatimondani@gmail.com>
2024-11-25 16:52:05 -0800

    fix: Add custom MIME type handling and simplify build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'configure-response-headers',
+      configureServer(server) {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Content-Type', 'application/javascript');
+          next();
+        });
+      }
+    }
+  ],
   base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 })
