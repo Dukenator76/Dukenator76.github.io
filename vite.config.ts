eb2867d27de39523d3ce2cb000dbe2a8b2067738
Dukenator76 <ducatimondani@gmail.com>
2024-11-25 16:46:41 -0800

    fix: Update build configuration for relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'es2020'
-    }
-  },
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+    assetsDir: 'assets'
   }
 })
